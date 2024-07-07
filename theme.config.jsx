33329debcf9eb853c3877550b796ec41f971a5f4
浜田　俊祐 <hamashun0820@icloud.com>
2024-07-07 11:44:22 +0900

    OGPを設定

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,19 @@
+import { useConfig } from "nextra-theme-docs";
+
 export default {
   logo: <span>My Nextra Documentation</span>,
   project: {
     link: "https://github.com/shuding/nextra",
   },
-  // ... other theme options
+  header: () => {
+    const { title } = useConfig();
+    return (
+      <>
+        <meta
+          name="og:image"
+          content={`https://nextra-ogp-sample.vercel.app/api/ogp?title=${title}`}
+        />
+      </>
+    );
+  },
 };
